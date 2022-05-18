ff22c2dd9c9bcbf8551eff803178237b51c9092d
Pedro Lucas <78425332+PedroLucas63@users.noreply.github.com>
2022-05-18 15:46:33 -0300

    Versão 1.0
    - Removido o comentário do código que não usava métodos e classes.
    - Traduzido os nomes dos métodos e variáveis com o objetivo de deixar todo o programa em uma só língua.

--- Program.cs
+++ Program.cs
@@ -1,32 +1,3 @@
-﻿/*
-//Criação de variáveis do tipo float:
-float numero1, numero2, soma, subtracao, multiplicacao, divisao, resto;
-
-//Escreve na tela sem a quebra de linha:
-Console.Write("Escreva um número: ");
-//Lê oque foi digitado na linha, passa para o tipo float e salva na variavel:
-numero1 = float.Parse(Console.ReadLine());
-
-Console.Write("Escreva outro número: ");
-numero2 = float.Parse(Console.ReadLine());
-
-//Faz as operações entre as duas variáveis e salva nas demais variáveis:
-soma = numero1 + numero2;
-subtracao = numero1 - numero2;
-multiplicacao = numero1 * numero2;
-divisao = numero1 / numero2;
-resto = numero1 % numero2;
-
-//Faz um cabeçalho inde vai ter 20 caracteres de igual (=):
-Console.WriteLine($"{new String('=', 20)}OPERAÇÕES{new String('=', 20)}\n");
-//Exibe as informações das operações aritméticas:
-Console.WriteLine($"{numero1} + {numero2} = {soma}");
-Console.WriteLine($"{numero1} - {numero2} = {subtracao}");
-Console.WriteLine($"{numero1} * {numero2} = {multiplicacao}");
-Console.WriteLine($"{numero1} / {numero2} = {divisao}");
-Console.WriteLine($"{numero1} % {numero2} = {resto}");
-*/
-
 //Abertura do projeto:
 namespace Operations
 {
@@ -37,94 +8,94 @@ namespace Operations
         static void Main(string[] args)
         {
             //Criação das variáveis:
-            string number1, number2, result;
+            string numero1, numero2, resultado;
 
             //Escrita do cabeçalho da aplicação:
             Console.WriteLine($"{new String('=', 20)}CALCULADORA{new String('=', 20)}");
             
             //Pede os dois números:
             Console.Write("Digite um número: ");
-            number1 = Console.ReadLine();
+            numero1 = Console.ReadLine();
             Console.Write("Digite outro número: ");
-            number2 = Console.ReadLine();
+            numero2 = Console.ReadLine();
 
             //Criação do referencial dos resultados: 
             Console.WriteLine($"\n{new String('=', 20)}RESULTADOS{new String('=', 20)}");
 
             //Execução da função Sum e escrita:
-            result = Sum(number1, number2);
-            Console.WriteLine(result);
+            resultado = Soma(numero1, numero2);
+            Console.WriteLine(resultado);
             
             //Execução da função Sub e escrita:
-            result = Sub(number1, number2);
-            Console.WriteLine(result);
+            resultado = Subtracao(numero1, numero2);
+            Console.WriteLine(resultado);
 
             //Execução da função Mult e escrita:
-            result = Mult(number1, number2);
-            Console.WriteLine(result);
+            resultado = Multiplicacao(numero1, numero2);
+            Console.WriteLine(resultado);
 
             //Execução da função Div e escrita:
-            result = Div(number1, number2);
-            Console.WriteLine(result);
+            resultado = Divisao(numero1, numero2);
+            Console.WriteLine(resultado);
 
             //Execução da função Mod e escrita:
-            result = Mod(number1, number2);
-            Console.WriteLine(result);
+            resultado = Modulo(numero1, numero2);
+            Console.WriteLine(resultado);
         }
 
         //Criação da função de soma que recebe 2 strings:
-        public static string Sum(string number1, string number2)
+        public static string Soma(string numero1, string numero2)
         {
             //Conversão das strings para float:
-            float number1_float = float.Parse(number1);
-            float number2_float = float.Parse(number2);
+            float numero1_float = float.Parse(numero1);
+            float numero2_float = float.Parse(numero2);
 
             //Retorno da String com o calculo:
-            return $"{number1_float} + {number2_float} = {number1_float + number2_float}";
+            return $"{numero1_float} + {numero2_float} = {numero1_float + numero2_float}";
         }
 
         //Criação da função de subtração que recebe 2 strings:
-        public static string Sub(string number1, string number2)
+        public static string Subtracao(string numero1, string numero2)
         {
             //Conversão das strings para float:
-            float number1_float = float.Parse(number1);
-            float number2_float = float.Parse(number2);
+            float numero1_float = float.Parse(numero1);
+            float numero2_float = float.Parse(numero2);
 
             //Retorno da String com o calculo:
-            return $"{number1_float} - {number2_float} = {number1_float - number2_float}";
+            return $"{numero1_float} - {numero2_float} = {numero1_float - numero2_float}";
         }
 
         //Criação da função de multiplicação que recebe 2 strings:
-        public static string Mult(string number1, string number2)
+        public static string Multiplicacao(string numero1, string numero2)
         {
             //Conversão das strings para float:
-            float number1_float = float.Parse(number1);
-            float number2_float = float.Parse(number2);
+            float numero1_float = float.Parse(numero1);
+            float numero2_float = float.Parse(numero2);
 
             //Retorno da String com o calculo:
-            return $"{number1_float} × {number2_float} = {number1_float * number2_float}";
+            return $"{numero1_float} × {numero2_float} = {numero1_float * numero2_float}";
         }
 
         //Criação da função de divisão que recebe 2 strings:
-        public static string Div(string number1, string number2)
+        public static string Divisao(string numero1, string numero2)
         {
             //Conversão das strings para float:
-            float number1_float = float.Parse(number1);
-            float number2_float = float.Parse(number2);
+            float numero1_float = float.Parse(numero1);
+            float numero2_float = float.Parse(numero2);
 
             //Retorno da String com o calculo:
-            return $"{number1_float} ÷ {number2_float} = {number1_float / number2_float}";
+            return $"{numero1_float} ÷ {numero2_float} = {numero1_float / numero2_float}";
         }
 
         //Criação da função de módulo que recebe 2 strings:
-        public static string Mod(string number1, string number2)
+        public static string Modulo(string numero1, string numero2)
         {
             //Conversão das strings para float:
-            float number1_float = float.Parse(number1);
-            float number2_float = float.Parse(number2);
+            float numero1_float = float.Parse(numero1);
+            float numero2_float = float.Parse(numero2);
 
             //Retorno da String com o calculo:
-            return $"{number1_float} % {number2_float} = {number1_float % number2_float}";
+            return $"{numero1_float} % {numero2_float} = {numero1_float % numero2_float}";
         }
 
     }
